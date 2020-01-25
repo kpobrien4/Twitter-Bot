bce18f627a064232686cda835a5ae705a56ee12b
Kevin O'Brien <kpobrien@buffalo.edu>
2020-01-25 11:46:51 -0500

    heroku

--- bot.py
+++ bot.py
@@ -1,11 +1,27 @@
 import tweepy
 from keys import *
+from os import environ
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
+import time
+from random import random
 
 
+INTERVAL = 60 * 60 * 6  # tweet every 6 hours
+# INTERVAL = 15  # tweet every 15 seconds
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+def hello():
+    return("Hello " + str(random()))
 
-api.update_status("Hello")
+
+while True:
+    print("Tweeting...")
+    hello = hello()
+    api.update_status(hello)
+    time.sleep(INTERVAL)
